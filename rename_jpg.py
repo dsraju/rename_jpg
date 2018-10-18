709b52b8b4de34c9e3848536a17af275680b1706
raju <dsraju@users.noreply.github.com>
2018-10-18 01:19:44 -0700

    Fixed an issue where the jpg files with missing exif data.

--- rename_jpg.py
+++ rename_jpg.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """
@@ -11,12 +11,16 @@ Solution:
 Rename all the jpeg files based on the datetime of the file.
 It rename all the files, recurlivel in a given directory.
  Default is the current directory.
+
+1.0.1 (2018-10-18 01:03:56 PDT)
+* Fixed an issue where the jpg files with missing exif data.
 """
 
 __author__ = "Raju Dantuluri"
 __license__ = "GPL"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
+import sys
 import pathlib
 import glob
 from PIL import Image
@@ -50,29 +54,34 @@ class Imageproc():
         types = ('*.jpg', '*.JPG', '*.jpeg', '*.JPEG')
         for f in types:
             filelist = list(glob.iglob(f'{self.dir}/**/{f}', recursive=True))
-            if len(filelist):
-                for file in filelist:
-                    src_file = pathlib.Path(file)
-                    img = Image.open(src_file)
-                    dt = img._getexif()[36868]
-                    img.close()
-                    del img
-                    dt = dt.replace(' ', '')
-                    dt = dt.replace(':', '')
-                    dest_file = src_file.parent / f'{dt}.jpg'
-                    if src_file == dest_file:
-                        continue
-                    if dest_file.exists():
-                        if self.verbose:
-                            print(
-                                f"skip - src:{src_file} dest:{dest_file} exists")
-                        continue
-                    src_file.rename(dest_file)
-                    print(f'{file} => {dest_file}')
+            for file in filelist:
+                src_file = pathlib.Path(file)
+                img = Image.open(src_file)
+                exif = img._getexif()
+                dt = exif[36868] if exif and 36868 in exif else None
+                img.close()
+                del img
+                if dt is None:
+                    print(f'{src_file}: found no exif data')
+                    continue
+                dt = dt.replace(' ', '')
+                dt = dt.replace(':', '')
+                dest_file = src_file.parent / f'{dt}.jpg'
+                if src_file == dest_file:
+                    continue
+                if dest_file.exists():
+                    if self.verbose:
+                        print(
+                            f"skip - src:{src_file} dest:{dest_file} exists")
+                    continue
+                src_file.rename(dest_file)
+                print(f'{file} => {dest_file}')
 
 
 if __name__ == '__main__':
-
+    if sys.version_info < (3, 0):
+        print('{} not supported.'.format(sys.version))
+        sys.exit()
     parser = argparse.ArgumentParser(description='Process image files.')
     parser.add_argument('--directory', required=False, default='.')
     parser.add_argument('--verbose', action="store_true", default=False)
